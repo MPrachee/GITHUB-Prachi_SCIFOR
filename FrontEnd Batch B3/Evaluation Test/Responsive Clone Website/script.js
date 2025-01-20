9011c756f5c5d0eccc37656f330f52f2256facf6
MPrachee <137384778+MPrachee@users.noreply.github.com>
2025-01-20 13:29:40 +0530

    Add files via upload

---
+++ FrontEnd Batch B3/Evaluation Test/Responsive Clone Website/script.js
@@ -0,0 +1,18 @@
+const container = document.querySelector('.testimonial-container');
+function scrollTestimonials(direction) {
+  const scrollAmount = 320;
+  container.scrollBy({ left: direction * scrollAmount, behavior: 'smooth' });
+}
+
+const scrollTopBtn = document.getElementById('scrollTopBtn');
+window.addEventListener('scroll', () => {
+  if (window.scrollY > 200) {
+    scrollTopBtn.style.display = 'block';
+  } else {
+    scrollTopBtn.style.display = 'none';
+  }
+});
+
+scrollTopBtn.addEventListener('click', () => {
+  window.scrollTo({ top: 0, behavior: 'smooth' });
+});
